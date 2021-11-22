10bc801cc1278594c950b3bdc5796bc3dbe318b1
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-22 15:28:49 +0200

    #3 - Status Codes display error message in case of wrong endpoint

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+// https://developer.mozilla.org/fr/docs/Web/HTTP/Status
+
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
 
 //  console.log(request, request.readyState);
-  if (request.readyState === 4){
+  if(request.readyState === 4 && request.status === 200) {
     console.log(request.responseText);  
+  }  else if(request.readyState === 4) {
+    console.log('could not fetch data');
   }
 });
 
