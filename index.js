7ee5d7cad6744a2e00d6a888b4730fda6eefe988
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-22 17:01:35 +0200

    #4 - Callback Functions to make an async function

--- index.js
+++ index.js
@@ -2,15 +2,34 @@
 
 const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
+function getTodos (callBack){
+  request.addEventListener('readystatechange', () => {
+  
+  //  console.log(request, request.readyState);
+    if(request.readyState === 4 && request.status === 200) {
+    callBack(undefined, request.responseText);
+    } else if(request.readyState === 4) {
+      callBack('could not fetch data', undefined);
+    }
+  });
+  
+  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+  request.send();
+};
 
-//  console.log(request, request.readyState);
-  if(request.readyState === 4 && request.status === 200) {
-    console.log(request.responseText);  
-  }  else if(request.readyState === 4) {
+console.log(1);
+console.log(2);
+
+getTodos((err, data) => {
+  console.log('CallBack Fired');
+  // console.log(err, data)
+  if(err){
     console.log('could not fetch data');
+  } else{
+    console.log(request.responseText);
+
   }
 });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+console.log(3);
+console.log(4);
