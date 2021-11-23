771c84275fb90ce6d4363877fc814cfeded7a721
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-23 16:42:31 +0200

    Use .catch method instead of having two callback function a argument

--- index.js
+++ index.js
@@ -47,13 +47,19 @@ function getTodos (resource, callBack){
 
 const getSomething = () => {
   return new Promise((resolve, reject) => {
-    resolve("some data"); // work like return, both can't work
+    // resolve("some data"); // work like return, both can't work
     reject("some error"); // once on of them match automatically it's skip the following one
   });
 };
 
-getSomething().then((data) => {
+// getSomething().then((data) => {
+//   console.log(data);
+// }, (err) => {
+//   console.log(err);
+// });
+
+getSomething().then(data => {
   console.log(data);
-}, (err) => {
+}).catch(err => {
   console.log(err);
 });
