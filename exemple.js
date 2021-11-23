e3b0abdfdf7ac0a52fe1be7bbf6f75e6396e69e4
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-23 17:51:04 +0200

    #7 Promises

--- exemple.js
+++ exemple.js
@@ -18,3 +18,52 @@ const getSomething = () => {
   }).catch(err => {
     console.log(err);
   });
+
+
+
+//   // https://developer.mozilla.org/fr/docs/Web/HTTP/Status
+
+// function getTodos (resource, callBack){
+//     const request = new XMLHttpRequest();
+  
+//     request.addEventListener('readystatechange', () => {
+//       if(request.readyState === 4 && request.status === 200) {
+//         const data = JSON.parse(request.responseText);
+//         callBack(undefined, data);
+//       }
+//       else if(request.readyState === 4) {
+//         callBack('could not fetch data', undefined);
+//       }
+//     });
+    
+//   //   request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+//     request.open('GET', resource);
+  
+//     request.send();
+//   };
+  
+//   //Nexting a request inside a callback function (data)
+  
+//   getTodos('todos/locki.json', (err, data) => {
+//     console.log('CallBack Fired');
+//     console.log(data)
+  
+//     getTodos('todos/mariot.json', (err, data) => {
+//       if(err){
+//         console.log('could not fetch data mariot');
+//       }
+//       else{
+//         console.log(data);
+//         getTodos('todos/zengi.json', (err, data) => {
+//           // if(err){
+//           //   console.log('could not fetch data zengi');
+//           // }
+//           // else{
+//             console.log(data);
+//           // }
+//         });
+//       }
+//     });
+//   });
+  
+  
--- index.js
+++ index.js
@@ -1,45 +1,36 @@
 // https://developer.mozilla.org/fr/docs/Web/HTTP/Status
 
-function getTodos (resource, callBack){
-  const request = new XMLHttpRequest();
-
-  request.addEventListener('readystatechange', () => {
-    if(request.readyState === 4 && request.status === 200) {
-      const data = JSON.parse(request.responseText);
-      callBack(undefined, data);
-    }
-    else if(request.readyState === 4) {
-      callBack('could not fetch data', undefined);
-    }
+function getTodos (resource){
+  return new Promise ((resolve, reject) => {
+    const request = new XMLHttpRequest();
+  
+    request.addEventListener('readystatechange', () => {
+      if(request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        // callBack(undefined, data);
+        resolve(data);
+      }
+      else if(request.readyState === 4) {
+        // callBack('could not fetch data', undefined);
+        reject('error getting resource');
+      }
+    });
+      // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+      request.open('GET', resource);
+      request.send();
   });
   
-//   request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-  request.open('GET', resource);
-
-  request.send();
 };
 
 //Nexting a request inside a callback function (data)
-
 // getTodos('todos/locki.json', (err, data) => {
 //   console.log('CallBack Fired');
 //   console.log(data)
-
-//   getTodos('todos/mariot.json', (err, data) => {
-//     if(err){
-//       console.log('could not fetch data mariot');
-//     }
-//     else{
-//       console.log(data);
-//       getTodos('todos/zengi.json', (err, data) => {
-//         // if(err){
-//         //   console.log('could not fetch data zengi');
-//         // }
-//         // else{
-//           console.log(data);
-//         // }
-//       });
-//     }
-//   });
 // });
+
+getTodos('todos/locki.json').then(data => {
+  console.log('promise resolved:', data);
+}).catch(err => {
+  console.log('promise rejcted:', err)
+});
 
