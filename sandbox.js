e102affca1a7c419e258699d3e9ece82920b915b
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-11-25 14:49:12 +0200

    #10 Async & Await

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,22 @@
 // fetch api
 
-fetch('todos/locki.json').then(response => {
-    console.log('resolved', response);
-    return response.json();
-}).then(data => {
-    console.log(data)
-}).catch(err => {
-    console.log('rejected', err)
-});
+// fetch('todos/locki.json').then(response => {
+//     console.log('resolved', response);
+//     return response.json(); // Return promise that can either be rejected or resolve
+// }).then(data => {
+//     console.log(data)
+// }).catch(err => {
+//     console.log('rejected', err)
+// });
+
+// async & await
+
+const getTodos = async () => {
+  const response = await fetch('todos/locki.json');
+  const data = await response.json();
+//   console.log(data);
+  return data;
+};
+
+getTodos()
+  .then(data => console.log('resolved:', data));
