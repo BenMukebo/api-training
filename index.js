bf9448f6730a154f00e9b7777e1063cda0ec64b8
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-04-12 16:51:29 +0200

    #3 Check the request status as well

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
   // console.log(request, request.readystate);
-  if(request.readyState === 4){
+  if(request.readyState === 4 && request.status === 200) {
     console.log(request.responseText);
+  } else if(request.readyState === 4) {
+    console.log('could not fetch the data');
   }
 })
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/');
 request.send();
