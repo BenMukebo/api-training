6921d7a2833b5ba53e22b32dd312e03bbd962bb3
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-04-12 16:20:54 +0200

    display the 4 fases of the request

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const request = new XMLHttpRequest();
 
+request.addEventListener('readystatechange', () => {
+  console.log(request, request.readystate)
+})
+
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
 request.send();
