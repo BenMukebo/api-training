c7bebcb2e3ef6c1817966394b0496cf31872bba5
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-04-12 16:26:43 +0200

    Dispaly responseText

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
-  console.log(request, request.readystate)
+  // console.log(request, request.readystate);
+  if(request.readyState === 4){
+    console.log(request.responseText);
+  }
 })
 
 request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
