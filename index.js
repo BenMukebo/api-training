5723a36d71166e3d0d716da026bb629c87596c65
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-04-13 23:55:56 +0200

    Make a http request to an html file w3school

--- index.js
+++ index.js
@@ -1,27 +1,50 @@
-const getTodos = (callback) => {
-  const request = new XMLHttpRequest();
+// const getTodos = (callback) => {
+//   const request = new XMLHttpRequest();
   
-  request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readystate);
-    if(request.readyState === 4 && request.status === 200) {
-      callback(undefined, request.responseText);
-      // console.log(request.responseText);
-    } else if(request.readyState === 4) {
-      callback('could not fetch the data', undefined);
-    }
-  })
+//   request.addEventListener('readystatechange', () => {
+//     // console.log(request, request.readystate);
+//     if(request.readyState === 4 && request.status === 200) {
+//       callback(undefined, request.responseText);
+//       // console.log(request.responseText);
+//     } else if(request.readyState === 4) {
+//       callback('could not fetch the data', undefined);
+//     }
+//   })
   
-  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-  request.send();
+//   request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+//   request.send();
+// }
+
+// getTodos((err, data) => {
+//   console.log('callback fired');
+
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log(data);
+//   }
+// });
+
+
+
+
+// JavaScript Callbacks W3School
+
+function myDisplayer(some) {
+  document.getElementById("demo").innerHTML = some;
 }
 
-getTodos((err, data) => {
-  console.log('callback fired');
-
-  if(err){
-    console.log(err);
-  } else {
-    console.log(data);
+function getFile(myCallback) {
+  let req = new XMLHttpRequest();
+  req.open('GET', "mycar.html");
+  req.onload = function() {
+    if (req.status == 200) {
+      myCallback(this.responseText);
+    } else {
+      myCallback("Error: " + req.status);
+    }
   }
-});
+  req.send();
+}
 
+getFile(myDisplayer);
