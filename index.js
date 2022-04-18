70863745d23920f06ef2caac96da4c2f93c03ad4
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-04-18 15:41:02 +0200

    #5 Real example using json file

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const getTodos = (callback) => {
     }
   })
   
-  request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+  // request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
+  request.open('GET', 'todos.json');
+
   request.send();
 }
 
